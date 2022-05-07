f5c0e96ee11e6172fb162fa2e98fd7317bb91aa9
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-08 00:24:04 +0300

    ft:database configuration - configures SQLAlchemy connection to database

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,23 @@
 from flask import Flask
 from config import config_options
+# making use of SQL ALchemy
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy() # an instance
 
 def create_app(config_name):
     
     app = Flask(__name__)
     
     
-     # Creating the app configurations
+    # Creating the app configurations
     app.config.from_object(config_options[config_name])
     
      # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    
+    #Initializing the extensions from flask
+    db.init_app(app)
 
     return app
