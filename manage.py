b515af16007ed8c486676a458cb097a2360ed6a2
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-08 01:18:44 +0300

    ts:databse-migration - tests database upgrade via flask migrate migrations

--- manage.py
+++ manage.py
@@ -1,10 +1,27 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from  flask_migrate import Migrate, MigrateCommand
+
+
+from app.models import User
+
+
+
 
 # Creating app instance
 app = create_app('development')
 
+
+
+
 manager = Manager(app)
-manager.add_command('server',Server) # launc the server to run application
+manager.add_command('server',Server) # launch the server to run application
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+#FlaskScript shell
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 if __name__ == '__main__':
     manager.run()
