a6703062fa3ada878465409121d484f3460c1ccb
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-08 15:52:22 +0300

    ft:login - implements user login

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
-from flask import render_template,url_for
+from flask import render_template,url_for, abort
 from . import main
+from ..models import User
 # we want to access the login functionality for some features eg voting and making a pitch
 from flask_login import login_required
 
@@ -12,4 +13,14 @@ def index():
     title = "pitch & pitch"
     
     return render_template('index.html',title=title)
+
+#The profile where users will view their previous pitches
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404) #stops a requests
+
+    return render_template("profile/profile.html", user = user)
 
--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
+from ast import Index
+import email
+from enum import unique
+
+from flask_login import UserMixin
 from . import db, login_manager
 #securing user passwords
 from werkzeug.security import generate_password_hash,check_password_hash
@@ -7,12 +12,13 @@ from . import login_manager
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model):
+class User( UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True) 
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True, index = True)
+    avatar = db.Column(db.String())
     password_secure = db.Column(db.String(255))
-    
     
     @property  #used to create a write only class property password
     def password(self):
@@ -20,11 +26,11 @@ class User(db.Model):
 
     @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        self.password_secure = generate_password_hash(password)
 
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+        return check_password_hash(self.password_secure,password)
     
     
     def __repr__(self):
