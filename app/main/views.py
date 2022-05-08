d220c12303b35c5d6c5f50ab7a197dbba96a6445
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-08 16:35:29 +0300

    ft:user-registration - implements user registration and login to allow user get a session

--- app/main/views.py
+++ app/main/views.py
@@ -22,5 +22,5 @@ def profile(uname):
     if user is None:
         abort(404) #stops a requests
 
-    return render_template("profile/profile.html", user = user)
+    return render_template("userProfile/profile.html", user = user)
 
