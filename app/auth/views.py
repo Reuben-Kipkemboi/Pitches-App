b0b6a2d286169741b10193eb5569fc47f4572085
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-10 05:29:04 +0300

    deployment to heroku

--- app/auth/views.py
+++ app/auth/views.py
@@ -17,7 +17,8 @@ def register():
         db.session.add(user)
         db.session.commit()
         
-        mail_message("Welcome to Pitch & Pitch, Pitch to Impress","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Pitch & Pitch","email/welcome_user",user.email,user=user)
+        
         return redirect(url_for('auth.login'))
     
     
