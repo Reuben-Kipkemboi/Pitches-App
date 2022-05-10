14a06bcf367b9fbf1bbf0a731f250245825237ff
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-05-10 21:04:57 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = "postgresql://dagmdgfiqthkke:3ae2085f0190fc7df62f37c701ea08f158a70b9936421ec2130f6a1faa17e9bb@ec2-34-231-177-125.compute-1.amazonaws.com:5432/de9lr0qedhl3tc"
 
 class DevConfig(Config):
     '''
